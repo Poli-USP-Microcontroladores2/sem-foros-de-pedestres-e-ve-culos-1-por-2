1cc194b446095698136c7df832cf02eed7f6a554
Bruno Mora <bruno.souza.mora77@gmail.com>
2025-11-04 17:02:41 -0300

    Versão 2 da implementação da sincronização

--- src/main.c
+++ src/main.c
@@ -13,25 +13,18 @@ LOG_MODULE_REGISTER(sem_mod, LOG_LEVEL_INF);
 // Mutex para controlar acesso aos LEDs
 K_MUTEX_DEFINE(led_mutex);
 
-// Semáforo binário para exclusão de acesso aos LEDs
-K_SEM_DEFINE(led_sem, 1, 1);
-
-// Semáforo para sincronizar farol com botão
-K_SEM_DEFINE(button_sem, 0, 1);
+// Semáforo para sincronização do verde
+K_SEM_DEFINE(green_sem, 1, 1);
 
 // Nodes do device tree
 #define LED0_NODE DT_ALIAS(led0)
 #define LED1_NODE DT_ALIAS(led1)
 #define LED2_NODE DT_ALIAS(led2)
-#define BUTTON_NODE DT_NODELABEL(user_button_0)
-#define OUT_PIN 1
+#define EXT_SIG_NODE DT_ALIAS(sw0)
 
-
-// Bool do Modo Noturno
+// Variáveis Globais
 bool nightMode = false;
-
-// Valor da entrada do Botão
-int value;
+bool external_active = false;
 
 #if DT_NODE_HAS_STATUS(LED0_NODE, okay)
 static const struct gpio_dt_spec ledG = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
@@ -51,86 +44,61 @@ static const struct gpio_dt_spec ledR = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
 #error "Unsupported board: led2 devicetree alias undefined"
 #endif
 
-#if DT_NODE_HAS_STATUS(BUTTON_NODE, okay)
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON_NODE, gpios);
+#if DT_NODE_HAS_STATUS(EXT_SIG_NODE, okay)
+static const struct gpio_dt_spec ext_sig = GPIO_DT_SPEC_GET(EXT_SIG_NODE, gpios);
 #else
-#error "Unsupported board: button devicetree alias undefined"
+#error "Unsupported board: ext_sig devicetree alias undefined"
 #endif
 
-static struct gpio_callback button_cb;
-// ----------------------------------------------------------------------------
-// Button ISR
-// ----------------------------------------------------------------------------
-void button_pressed(const struct device *dev, struct gpio_callback *cb,
-                    uint32_t pins)
+// ============================================================================
+// THREAD DE MONITORAMENTO DO SINAL EXTERNO
+// ============================================================================
+void external_signal_thread(void)
 {
-    value = true;
-}
+    while (1) {
 
-static void out_pin_set(int value)
-{
-    /* ledG.port é o device do controlador GPIO do ledG; usamos ele para
-       configurar/definir o pino OUT_PIN. */
-    if (ledG.port == NULL) {
-        return;
+        int val = gpio_pin_get_dt(&ext_sig);
+
+        if(val == 1) {
+            external_active = true;
+            k_sem_give(&green_sem);  // libera prioridade ao verde
+        } else {
+            external_active = false;
+        }
+
+        k_msleep(200);
     }
-    gpio_pin_set(ledG.port, OUT_PIN, value);
-}
-
-
-void force_red(void)
-{
-    k_mutex_lock(&led_mutex, K_FOREVER);
-
-    LOG_INF("Travessia requisitada! Interrompendo ciclo.");
-
-    gpio_pin_set_dt(&ledG, 0);
-    gpio_pin_set_dt(&ledB, 0);
-    gpio_pin_set_dt(&ledR, 1);
-   /* Seta o pino externo em HIGH enquanto o vermelho está ativo */
-    out_pin_set(1);
-
-    // tempo de travessia
-    k_msleep(4000);
-
-    value = false;
-
-    /* Volta pino externo para LOW */
-    out_pin_set(0);
-
-    k_mutex_unlock(&led_mutex);
 }
 
 // ============================================================================
 // THREAD DO LED VERDE
 // ============================================================================
-
 void G_LED(void)
 {
     for (;;) {
-        if(nightMode == false) {
 
-            k_sem_take(&led_sem, K_FOREVER);
+        // Espera permissão da sincronização
+        k_sem_take(&green_sem, K_FOREVER);
 
-            LOG_INF("Semáforo Verde");
-            gpio_pin_set_dt(&ledG, 1);
-            gpio_pin_set_dt(&ledR, 0);
-            gpio_pin_set_dt(&ledB, 0);
-            out_pin_set(0);
+        k_mutex_lock(&led_mutex, K_FOREVER);
 
-            if (value) {
-                force_red();
-                 LOG_INF("Voltando o Ciclo.");
-                 value = 0;
-            }
+        LOG_INF("Semáforo Verde");
+        gpio_pin_set_dt(&ledG, 1);
+        gpio_pin_set_dt(&ledR, 0);
+        gpio_pin_set_dt(&ledB, 0);
 
-            k_msleep(3000);
+        k_mutex_unlock(&led_mutex);
 
-            k_sem_give(&led_sem);
+        k_msleep(3000);
 
-        } else {
-            k_sleep(K_FOREVER);
+        k_sem_give(&green_sem);
+
+        // Se ainda há sinal externo, força verde novamente
+        if (external_active) {
+            continue;
         }
+
+        k_msleep(1000);
     }
 }
 
@@ -141,141 +109,60 @@ void R_LED(void)
 {
     for (;;) {
 
-        if (value == 1) {
-            k_sem_take(&led_sem, K_FOREVER);
-
-            LOG_INF("Botão Pressionado");
-            gpio_pin_set_dt(&ledR, 1);
-            gpio_pin_set_dt(&ledG, 0);
-            gpio_pin_set_dt(&ledB, 0);
-            out_pin_set(1);
-
-            k_msleep(4000);
-
-           out_pin_set(0);
-
-            k_sem_give(&led_sem);
+        // Se há prioridade externa, pula o vermelho
+        if (external_active) {
+            k_msleep(500);
+            continue;
         }
 
-        if(nightMode == false){
+        k_sem_take(&green_sem, K_FOREVER);
 
-            k_sem_take(&led_sem, K_FOREVER);
+        k_mutex_lock(&led_mutex, K_FOREVER);
 
-            LOG_INF("Semáforo Vermelho");
-            gpio_pin_set_dt(&ledR, 1);
-            gpio_pin_set_dt(&ledG, 0);
-            gpio_pin_set_dt(&ledB, 0);
-            out_pin_set(1);
+        LOG_INF("Semáforo Vermelho");
+        gpio_pin_set_dt(&ledR, 1);
+        gpio_pin_set_dt(&ledG, 0);
+        gpio_pin_set_dt(&ledB, 0);
 
-            if (value) {
-                force_red();
-                 LOG_INF("Voltando o Ciclo.");
-                value = 0;
-            }
+        k_mutex_unlock(&led_mutex);
 
+        k_msleep(3000);
 
-            k_msleep(4000);
+        k_sem_give(&green_sem);
 
-            out_pin_set(0);
-
-            k_sem_give(&led_sem);
-
-            k_msleep(4000);
-
-        } else {
-            k_sleep(K_FOREVER);
-        }
+        k_msleep(1000);
     }
 }
 
-// ============================================================================
-// THREAD DO LED AMARELO
-// ============================================================================
-void Y_LED(void)
-{
-    for (;;) {
-        if(nightMode == false){
-
-            k_sem_take(&led_sem, K_FOREVER);
-
-            LOG_INF("Semáforo Amarelo");
-            gpio_pin_set_dt(&ledR, 1);
-            gpio_pin_set_dt(&ledG, 1);
-            gpio_pin_set_dt(&ledB, 0);
-            out_pin_set(1);
-
-            if (value) {
-                force_red();
-                 LOG_INF("Voltando o Ciclo.");
-                value = 0;
-            }
-
-            k_msleep(1000);
-
-            out_pin_set(0);
-            k_sem_give(&led_sem);
-
-            k_msleep(3000);
-
-        } else {
-
-            LOG_INF("Modo Noturno");
-            gpio_pin_toggle_dt(&ledR);
-            gpio_pin_toggle_dt(&ledG);
-            k_msleep(1000);
-        }
-    }
-}
 // ============================================================================
 // DEFINIÇÃO DE THREADS
 // ============================================================================
-K_THREAD_DEFINE(Green_LedT, 512, G_LED, NULL, NULL, NULL, 6, 0, 0);
-K_THREAD_DEFINE(Red_LedT,   512, R_LED, NULL, NULL, NULL, 5, 0, 0);
-K_THREAD_DEFINE(Yellow_LedT,   512, Y_LED, NULL, NULL, NULL, 7, 0, 0);
+K_THREAD_DEFINE(GreenLedT, 512, G_LED, NULL, NULL, NULL, 6, 0, 0);
+K_THREAD_DEFINE(Red_LedT,   512, R_LED, NULL, NULL, NULL, 7, 0, 0);
+K_THREAD_DEFINE(ExtSigT,    512, external_signal_thread, NULL, NULL, NULL, 5, 0, 0);
 
 // ============================================================================
 // MAIN
 // ============================================================================
 void main(void)
 {
-    int ret;
-
     // Verifica dispositivos
     if (!gpio_is_ready_dt(&ledG) || !gpio_is_ready_dt(&ledR) || !gpio_is_ready_dt(&ledB)) {
         LOG_ERR("ERRO: LED device not ready!");
         return;
     }
 
-    if (!gpio_is_ready_dt(&button)) {
-    return;
-    }
-
-    ret = gpio_pin_configure(ledG.port, OUT_PIN, GPIO_OUTPUT_INACTIVE);
-    if (ret < 0) {
-        LOG_ERR("Falha ao configurar OUT_PIN (%d): %d", OUT_PIN, ret);
+    if (!gpio_is_ready_dt(&ext_sig)) {
+        LOG_ERR("ERRO: External pin not ready!");
         return;
     }
-
 
     // Configura pinos
     gpio_pin_configure_dt(&ledG, GPIO_OUTPUT_INACTIVE);
     gpio_pin_configure_dt(&ledR, GPIO_OUTPUT_INACTIVE);
     gpio_pin_configure_dt(&ledB, GPIO_OUTPUT_INACTIVE);
-
-    
-    gpio_pin_interrupt_configure_dt(&button,
-        GPIO_INT_EDGE_TO_ACTIVE);
-
-    gpio_init_callback(&button_cb, button_pressed,
-                       BIT(button.pin));
-
-    gpio_add_callback(button.port, &button_cb);
-
+    gpio_pin_configure_dt(&ext_sig, GPIO_INPUT | GPIO_PULL_UP);
 
     LOG_INF("Sistema Iniciado");
     LOG_INF("Semáforo Pronto!");
-    while(1){
-        value = gpio_pin_get_dt(&button);
-         k_msleep(100);
-    }
 }

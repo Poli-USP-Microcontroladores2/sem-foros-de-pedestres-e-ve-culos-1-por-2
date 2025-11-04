69bfaaf64c02240d74eeb12c7581ae058794e558
Bruno Mora <bruno.souza.mora77@gmail.com>
2025-11-04 14:41:23 -0300

    Versão 1 da implementação da sincronização do farol de pedestres com o de carros

--- src/main.c
+++ src/main.c
@@ -12,18 +12,19 @@ LOG_MODULE_REGISTER(sem_mod, LOG_LEVEL_INF);
 
 // Mutex para controlar acesso aos LEDs
 K_MUTEX_DEFINE(led_mutex);
-// Semáforo Binário para sincronizar os faróis
-K_SEM_DEFINE(led_sem, 0, 1);
+// Sinal externo solicitando prioridade
+K_SEM_DEFINE(ext_req_sem, 0, 1);
+K_SEM_DEFINE(green_sem, 1, 1);
 
 // Nodes do device tree
 #define LED0_NODE DT_ALIAS(led0)
 #define LED1_NODE DT_ALIAS(led1)
 #define LED2_NODE DT_ALIAS(led2)
-#define BUTTON_NODE DT_NODELABEL(user_button_0)
+#define EXT_SIG_NODE DT_ALIAS(sw1)   // <-- novo input
 
 // Variáveis Globais
 bool nightMode = false;
-int value;
+bool external_active = false;
 
 #if DT_NODE_HAS_STATUS(LED0_NODE, okay)
 static const struct gpio_dt_spec ledG = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
@@ -43,22 +44,28 @@ static const struct gpio_dt_spec ledR = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
 #error "Unsupported board: led2 devicetree alias undefined"
 #endif
 
-#if DT_NODE_HAS_STATUS(BUTTON_NODE, okay)
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON_NODE, gpios);
+#if DT_NODE_HAS_STATUS(EXT_SIG_NODE, okay)
+static const struct gpio_dt_spec ext_sig = GPIO_DT_SPEC_GET(EXT_SIG_NODE, gpios);
 #else
-#error "Unsupported board: button devicetree alias undefined"
+#error "Unsupported board: ext_sig devicetree alias undefined"
 #endif
 
-static struct gpio_callback button_cb;
-
-// ----------------------------------------------------------------------------
-// Button ISR
-// ----------------------------------------------------------------------------
-
-void button_pressed(const struct device *dev, struct gpio_callback *cb,
-                    uint32_t pins)
+void external_signal_thread(void)
 {
-    value = true;
+    while(1) {
+
+        int val = gpio_pin_get_dt(&ext_sig);
+
+        if(val == 1) { // solicita prioridade
+            external_active = true;
+            k_sem_give(&ext_req_sem);
+        } else {
+            external_active = false;
+            k_sem_reset(&ext_req_sem); // libera prioridade
+        }
+
+        k_msleep(100);
+    }
 }
 
 // ============================================================================
@@ -67,6 +74,9 @@ void button_pressed(const struct device *dev, struct gpio_callback *cb,
 void G_LED(void)
 {
     for (;;) {
+       if(external_active) {
+         // Pede prioridade sobre o verde
+         k_sem_take(&green_sem, K_FOREVER);
         if(nightMode == false){
 		k_msleep(4000);
 
@@ -79,11 +89,14 @@ void G_LED(void)
 
         k_mutex_unlock(&led_mutex);
 
+        k_sem_give(&green_sem);
+
 		k_msleep(2000);
         }
         else {
             k_sleep(K_FOREVER);
         }
+    }
     }
 }
 
@@ -93,6 +106,8 @@ void G_LED(void)
 void R_LED(void)
 {
     for (;;) {
+    k_sem_take(&green_sem, K_FOREVER);
+
 	if (nightMode == false){
 		k_msleep(2000);
 
@@ -105,6 +120,7 @@ void R_LED(void)
 
         k_mutex_unlock(&led_mutex);
 
+        k_sem_give(&green_sem);
         k_msleep(4000);
 
     }
@@ -123,8 +139,9 @@ void R_LED(void)
 // ============================================================================
 // DEFINIÇÃO DE THREADS
 // ============================================================================
-K_THREAD_DEFINE(Green_LedT, 512, G_LED, NULL, NULL, NULL, 7, 0, 0);
+K_THREAD_DEFINE(GreenLedT, 512, G_LED, NULL, NULL, NULL, 7, 0, 0);
 K_THREAD_DEFINE(Red_LedT,   512, R_LED, NULL, NULL, NULL, 6, 0, 0);
+K_THREAD_DEFINE(ExtSigT,    512, external_signal_thread, NULL, NULL, NULL, 5, 0, 0);
 
 // ============================================================================
 // MAIN
@@ -139,10 +156,16 @@ void main(void)
         return;
     }
 
+    if (!gpio_is_ready_dt(&ext_sig)) {
+        LOG_ERR("ERRO: External pin not ready!");
+        return;
+    }
+
     // Configura pinos
     gpio_pin_configure_dt(&ledG, GPIO_OUTPUT_INACTIVE);
     gpio_pin_configure_dt(&ledR, GPIO_OUTPUT_INACTIVE);
     gpio_pin_configure_dt(&ledB, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&ext_sig, GPIO_INPUT | GPIO_PULL_DOWN);
 
     LOG_INF("Sistema Iniciado");
     LOG_INF("Semáforo Pronto!");

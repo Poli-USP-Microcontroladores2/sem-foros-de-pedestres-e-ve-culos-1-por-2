a23102ef56134272121d63ee254998eb12f6a935
Bruno Mora <bruno.souza.mora77@gmail.com>
2025-11-04 14:15:57 -0300

    Versão do código de pedestre com Modo noturno

--- src/main.c
+++ src/main.c
@@ -12,12 +12,8 @@ LOG_MODULE_REGISTER(sem_mod, LOG_LEVEL_INF);
 
 // Mutex para controlar acesso aos LEDs
 K_MUTEX_DEFINE(led_mutex);
-
-// Semáforo binário para exclusão de acesso aos LEDs
-K_SEM_DEFINE(led_sem, 1, 1);
-
-// Semáforo para sincronizar farol com botão
-K_SEM_DEFINE(button_sem, 0, 1);
+// Semáforo Binário para sincronizar os faróis
+K_SEM_DEFINE(led_sem, 0, 1);
 
 // Nodes do device tree
 #define LED0_NODE DT_ALIAS(led0)
@@ -25,11 +21,8 @@ K_SEM_DEFINE(button_sem, 0, 1);
 #define LED2_NODE DT_ALIAS(led2)
 #define BUTTON_NODE DT_NODELABEL(user_button_0)
 
-
-// Bool do Modo Noturno
+// Variáveis Globais
 bool nightMode = false;
-
-// Valor da entrada do Botão
 int value;
 
 #if DT_NODE_HAS_STATUS(LED0_NODE, okay)
@@ -61,57 +54,34 @@ static struct gpio_callback button_cb;
 // ----------------------------------------------------------------------------
 // Button ISR
 // ----------------------------------------------------------------------------
+
 void button_pressed(const struct device *dev, struct gpio_callback *cb,
                     uint32_t pins)
 {
     value = true;
 }
 
-void force_red(void)
-{
-    k_mutex_lock(&led_mutex, K_FOREVER);
-
-    LOG_INF("Travessia requisitada! Interrompendo ciclo.");
-
-    gpio_pin_set_dt(&ledG, 0);
-    gpio_pin_set_dt(&ledB, 0);
-    gpio_pin_set_dt(&ledR, 1);
-
-    // tempo de travessia
-    k_msleep(4000);
-
-    value = false;
-
-    k_mutex_unlock(&led_mutex);
-}
-
 // ============================================================================
 // THREAD DO LED VERDE
 // ============================================================================
-
 void G_LED(void)
 {
     for (;;) {
-        if(nightMode == false) {
+        if(nightMode == false){
+		k_msleep(4000);
 
-            k_sem_take(&led_sem, K_FOREVER);
+        k_mutex_lock(&led_mutex, K_FOREVER);
 
-            LOG_INF("Semáforo Verde");
-            gpio_pin_set_dt(&ledG, 1);
-            gpio_pin_set_dt(&ledR, 0);
-            gpio_pin_set_dt(&ledB, 0);
+        LOG_INF("Semáforo Verde");
+        gpio_pin_set_dt(&ledG, 1);
+        gpio_pin_set_dt(&ledR, 0);
+        gpio_pin_set_dt(&ledB, 0);
 
-            if (value) {
-                force_red();
-                 LOG_INF("Voltando o Ciclo.");
-                 value = 0;
-            }
+        k_mutex_unlock(&led_mutex);
 
-            k_msleep(3000);
-
-            k_sem_give(&led_sem);
-
-        } else {
+		k_msleep(2000);
+        }
+        else {
             k_sleep(K_FOREVER);
         }
     }
@@ -123,90 +93,38 @@ void G_LED(void)
 void R_LED(void)
 {
     for (;;) {
+	if (nightMode == false){
+		k_msleep(2000);
 
-        if (value == 1) {
-            k_sem_take(&led_sem, K_FOREVER);
+		k_mutex_lock(&led_mutex, K_FOREVER);
 
-            LOG_INF("Botão Pressionado");
-            gpio_pin_set_dt(&ledR, 1);
-            gpio_pin_set_dt(&ledG, 0);
-            gpio_pin_set_dt(&ledB, 0);
+        LOG_INF("Semáforo Vermelho");
+        gpio_pin_set_dt(&ledR, 1);
+        gpio_pin_set_dt(&ledG, 0);
+        gpio_pin_set_dt(&ledB, 0);
 
-            k_msleep(4000);
+        k_mutex_unlock(&led_mutex);
 
-            k_sem_give(&led_sem);
-        }
+        k_msleep(4000);
 
-        if(nightMode == false){
-
-            k_sem_take(&led_sem, K_FOREVER);
-
-            LOG_INF("Semáforo Vermelho");
-            gpio_pin_set_dt(&ledR, 1);
-            gpio_pin_set_dt(&ledG, 0);
-            gpio_pin_set_dt(&ledB, 0);
-
-            if (value) {
-                force_red();
-                 LOG_INF("Voltando o Ciclo.");
-                value = 0;
-            }
-
-
-            k_msleep(4000);
-
-            k_sem_give(&led_sem);
-
-            k_msleep(4000);
-
-        } else {
-            k_sleep(K_FOREVER);
-        }
+    }
+    else{
+        k_mutex_lock(&led_mutex, K_FOREVER);
+         LOG_INF("Modo Noturno");
+        gpio_pin_set_dt(&ledG, 0);
+        gpio_pin_set_dt(&ledB, 0);
+        gpio_pin_toggle_dt(&ledR);
+        k_mutex_unlock(&led_mutex);
+        k_msleep(1000);
+    }
     }
 }
 
-// ============================================================================
-// THREAD DO LED AMARELO
-// ============================================================================
-void Y_LED(void)
-{
-    for (;;) {
-        if(nightMode == false){
-
-            k_sem_take(&led_sem, K_FOREVER);
-
-            LOG_INF("Semáforo Amarelo");
-            gpio_pin_set_dt(&ledR, 1);
-            gpio_pin_set_dt(&ledG, 1);
-            gpio_pin_set_dt(&ledB, 0);
-
-            if (value) {
-                force_red();
-                 LOG_INF("Voltando o Ciclo.");
-                value = 0;
-            }
-
-            k_msleep(1000);
-
-            k_sem_give(&led_sem);
-
-            k_msleep(3000);
-
-        } else {
-
-            LOG_INF("Modo Noturno");
-            gpio_pin_toggle_dt(&ledR);
-            gpio_pin_toggle_dt(&ledG);
-            k_msleep(1000);
-        }
-    }
-}
 // ============================================================================
 // DEFINIÇÃO DE THREADS
 // ============================================================================
-K_THREAD_DEFINE(Green_LedT, 512, G_LED, NULL, NULL, NULL, 6, 0, 0);
-K_THREAD_DEFINE(Red_LedT,   512, R_LED, NULL, NULL, NULL, 5, 0, 0);
-K_THREAD_DEFINE(Yellow_LedT,   512, Y_LED, NULL, NULL, NULL, 7, 0, 0);
+K_THREAD_DEFINE(Green_LedT, 512, G_LED, NULL, NULL, NULL, 7, 0, 0);
+K_THREAD_DEFINE(Red_LedT,   512, R_LED, NULL, NULL, NULL, 6, 0, 0);
 
 // ============================================================================
 // MAIN
@@ -221,30 +139,11 @@ void main(void)
         return;
     }
 
-    if (!gpio_is_ready_dt(&button)) {
-    return;
-    }
-
-
     // Configura pinos
     gpio_pin_configure_dt(&ledG, GPIO_OUTPUT_INACTIVE);
     gpio_pin_configure_dt(&ledR, GPIO_OUTPUT_INACTIVE);
     gpio_pin_configure_dt(&ledB, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_configure_dt(&button, GPIO_INPUT | GPIO_PULL_UP);
-
-    gpio_pin_interrupt_configure_dt(&button,
-        GPIO_INT_EDGE_TO_ACTIVE);
-
-    gpio_init_callback(&button_cb, button_pressed,
-                       BIT(button.pin));
-
-    gpio_add_callback(button.port, &button_cb);
-
 
     LOG_INF("Sistema Iniciado");
     LOG_INF("Semáforo Pronto!");
-    while(1){
-        value = gpio_pin_get_dt(&button);
-         k_msleep(100);
-    }
 }
